ac3180cb6ca5f2578f2d04956144498ba727adf2
Christopher Ray <vde435@gmail.com>
2021-09-20 10:10:03 -0500

    Update
    Trying to print any size 2D array.

--- printmat.c
+++ printmat.c
@@ -8,16 +8,16 @@ void printmat(int, int);
 int main(void){
 	int rows = 7;
 	int cols = 7;
-	int x[7][7] = { {0,0,77,88,99,145, 400}, {0,0,0,0,0,0,0}}; //all elements of matrix are zero
+	int x[7][7] = { {0,0,77,88,99,145, 400}, {0,0,0,0,0,0,0}};
 	
-	for(int i = 0; i < 7; i++){
-		for(int j = 0; j < 7; j++){
-			printf("%d\n ",x[i][j]); 
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			x[i][j] = 0; 
 		}
 	}
 
-	//printf("%s", "\n\n\n");
-	//printmat(7, 7);	
+	printf("%d", x[6][6]);
+	
 }
 
 void printmat(int rows, int cols){
